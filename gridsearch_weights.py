3fb80533e98a293f5aeee0960b46aeea0e1a44e9
preacher6 <juafelgrajalesg@gmail.com>
2020-12-10 11:06:56 -0500

    searches

---
+++ gridsearch_weights.py
@@ -0,0 +1,1 @@
+data\pima-indians-diabetes.data.csv
--- pytorch_example1.py
+++ pytorch_example1.py
@@ -88,3 +88,35 @@ for epoch in range(3): # 3 full passes over the data
         loss.backward()  # apply this loss backwards thru the network's parameters
         optimizer.step()  # attempt to optimize weights to account for loss/gradients
     print(loss)  # print loss. We hope loss (a measure of wrong-ness) declines! 
+    
+#%%
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in testset:
+        X, y = data
+        output = net(X.view(-1,784))
+        #print(output)
+        for idx, i in enumerate(output):
+            #print(torch.argmax(i), y[idx])
+            if torch.argmax(i) == y[idx]:
+                correct += 1
+            total += 1
+
+print("Accuracy: ", round(correct/total, 3))
+
+#%%
+import matplotlib.pyplot as plt
+
+plt.imshow(X[0].view(28,28))
+plt.show()
+print(torch.argmax(net(X[0].view(-1,784))[0]))
+#%% A pedal
+a_featureset = X[0]
+reshaped_for_network = a_featureset.view(-1,784) # 784 b/c 28*28 image resolution.
+output = net(reshaped_for_network) #output will be a list of network predictions.
+first_pred = output[0]
+print(first_pred)
+biggest_index = torch.argmax(first_pred)
+print(biggest_index)

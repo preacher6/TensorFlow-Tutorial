28b73354f4c5aeeb735cb3bdb9caf914b9ae8a69
preacher6 <juafelgrajalesg@gmail.com>
2020-12-11 11:39:22 -0500

    all grids

--- gridsearch_numneur.py
+++ gridsearch_numneur.py
@@ -1,15 +1,18 @@
-# Use scikit-learn to grid search the weight initialization
+# Use scikit-learn to grid search the number of neurons
 import numpy
 from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
+from keras.constraints import maxnorm
 # Function to create model, required for KerasClassifier
-def create_model(init_mode='uniform'):
+def create_model(neurons=1):
 	# create model
 	model = Sequential()
-	model.add(Dense(12, input_dim=8, kernel_initializer=init_mode, activation='relu'))
-	model.add(Dense(1, kernel_initializer=init_mode, activation='sigmoid'))
+	model.add(Dense(neurons, input_dim=8, kernel_initializer='uniform', activation='linear', kernel_constraint=maxnorm(4)))
+	model.add(Dropout(0.2))
+	model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 	# Compile model
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
@@ -24,8 +27,8 @@ Y = dataset[:,8]
 # create model
 model = KerasClassifier(build_fn=create_model, epochs=100, batch_size=10, verbose=0)
 # define the grid search parameters
-init_mode = ['uniform', 'lecun_uniform', 'normal', 'zero', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
-param_grid = dict(init_mode=init_mode)
+neurons = [1, 5, 10, 15, 20, 25, 30]
+param_grid = dict(neurons=neurons)
 grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
 grid_result = grid.fit(X, Y)
 # summarize results
--- grisearch_actfun.py
+++ grisearch_actfun.py
@@ -1,15 +1,15 @@
-# Use scikit-learn to grid search the weight initialization
+# Use scikit-learn to grid search the activation function
 import numpy
 from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 # Function to create model, required for KerasClassifier
-def create_model(init_mode='uniform'):
+def create_model(activation='relu'):
 	# create model
 	model = Sequential()
-	model.add(Dense(12, input_dim=8, kernel_initializer=init_mode, activation='relu'))
-	model.add(Dense(1, kernel_initializer=init_mode, activation='sigmoid'))
+	model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation=activation))
+	model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 	# Compile model
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
@@ -24,8 +24,8 @@ Y = dataset[:,8]
 # create model
 model = KerasClassifier(build_fn=create_model, epochs=100, batch_size=10, verbose=0)
 # define the grid search parameters
-init_mode = ['uniform', 'lecun_uniform', 'normal', 'zero', 'glorot_normal', 'glorot_uniform', 'he_normal', 'he_uniform']
-param_grid = dict(init_mode=init_mode)
+activation = ['softmax', 'softplus', 'softsign', 'relu', 'tanh', 'sigmoid', 'hard_sigmoid', 'linear']
+param_grid = dict(activation=activation)
 grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=3)
 grid_result = grid.fit(X, Y)
 # summarize results
